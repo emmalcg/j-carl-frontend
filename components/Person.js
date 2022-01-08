f1e56b3412bff2212cd0b6fd7da2f4679eea3b1c
Emma Green <emmalcgreen@gmail.com>
2022-01-08 07:49:51 -0500

    markdown text working

--- components/Person.js
+++ components/Person.js
@@ -1,4 +1,5 @@
 import MyImage from "./MyImage"
+import ReactMarkdown from 'react-markdown'
 
 export default function Person({person}) {
 
@@ -6,11 +7,11 @@ export default function Person({person}) {
 //  const { API_URL } = process.env
 //artwork.media.data.map(image => {console.log(image.attributes.url)})
 
-  console.log(person)
+  console.log(person.bio)
   return (
     <li>
       <h3 className="sr-only">{person.firstName} {person.lastName}</h3>
-      <p>{person.bio}</p>
+      <ReactMarkdown>{person.bio}</ReactMarkdown>
       {/*<div>
         {
           artwork.media.data.map(image => <MyImage src={image.attributes.url} alt={image.attributes.caption}/>)
