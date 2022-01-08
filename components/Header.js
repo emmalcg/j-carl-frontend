0bf22ff327b9e6c1d922ed312a1177ca4348bb27
Emma Green <emmalcgreen@gmail.com>
2022-01-08 09:52:44 -0500

    Revert "wasn't loading added nav items bt loading weird...."
    This reverts commit b811aebccb13f5da5e4b210963b23b927455f936.

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,7 @@
-import NavItem from './NavItem';
-
 export default function Header() {
-  //const linkItems = [
-  //  {title: 'Home', link:'/rfq'},
-  //  {title: 'Statement', link:'/rfq-statment'},
-  //  {title: 'Images', link:'/rfq-work'},
-  //  {title: 'Team', link:'/rfq-team'}
-  //]
-  //console.log(linkItems)
   return (
-    <Header>
-      <div>
-        <h1 className="text-1xl font-bold underline">James Carl Studio | <span>RFQ</span></h1>
-      </div>
-      <nav>
-        <ul>
-          {/*{linkItems.map(item => <NavItem key={item.title} title={item.title} link={item.link}/>)}*/}
-        </ul>
-      </nav>
-
-    </Header>
+    <div>
+      <h1 className="text-1xl font-bold underline">James Carl Studio | <span>RFQ</span></h1>
+    </div>
   )
 }
--- components/NavItem.js
+++
@@ -1,9 +0,0 @@
-export default function NavItem({ title, link }) {
-
-  return (
-    <li>
-      <a href={link}>{title}</a>
-
-    </li>
-  )
-}
