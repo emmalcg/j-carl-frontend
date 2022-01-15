2da89f86a1abf966e52e7ce6476ca559095c7ca8
Emma Green <emmalcgreen@gmail.com>
2022-01-15 18:26:36 -0500

    bleh

--- components/MyImage.js
+++ components/MyImage.js
@@ -4,9 +4,18 @@ export default function MyImage ({src, alt}) {
 
   return (
     <Image 
+<<<<<<< Updated upstream
       src={src}
       alt="{alt}"
       layout="fill"
+=======
+      src={image.image.url}
+      alt={image.image.caption}
+      width={image.image.width}
+      height={image.image.height}
+      layout="responsive"
+      loading="eager"
+>>>>>>> Stashed changes
     />
   )
 }
--- pages/rfq-statement.js
+++ pages/rfq-statement.js
@@ -1,4 +1,47 @@
-function Statement() {
-  return 'statement'
+import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import ReactMarkdown from 'react-markdown'
+import Image from 'next/image'
+
+function Statement({ statement }) {
+  const img = statement.data.attributes.statementImage.data.attributes
+  const mobileImg = statement.data.attributes.statementImageMobile.data.attributes
+  return (
+    <section className="flex flex-col lg:flex-row mb-2">
+      <article className="pr-6 prose">
+        <h2>Statement</h2>
+        <ReactMarkdown>
+          {statement.data.attributes.statement}
+        </ReactMarkdown>
+      </article>
+      <div className="hidden lg:block order-last">
+        <div className="relative min-w-[320px] max-w-[320px] max-h-[1000px] min-h-[1000px]">
+          <Image 
+              src={img.url}
+              alt={img.caption}
+              layout="fill"
+              objectFit="cover"
+              priority="true"
+            />
+        </div>
+        <div className="text-right text-xs">
+          <ReactMarkdown>{statement.data.attributes.statementImageCaption}</ReactMarkdown>
+        </div>
+      </div>
+      <div className="order-first lg:hidden">
+        <div className="relative h-[170px]">
+          <Image 
+              src={mobileImg.url}
+              alt={mobileImg.caption}
+              layout="fill"
+              objectFit="cover"
+              priority="true"
+            />
+        </div>
+        <div className="text-right text-xs">
+          <ReactMarkdown>{statement.data.attributes.statementImageCaption}</ReactMarkdown>
+        </div>
+      </div>
+    </section>
+  )
 }
 export default Statement
