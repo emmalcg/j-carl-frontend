6d1282f07c44d7bc16b0064a318ef9f9ed602a49
Emma Green <emmalcgreen@gmail.com>
2022-01-16 08:07:06 -0500

    mobile nav fix

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -48,7 +48,7 @@ export default function AppHeader() {
       </h1>
       { isRFQ && (
 
-        <nav>
+        <nav className="grow">
           <ul className="hidden sm:flex">
             {
               rfqNav.map(item => {
@@ -63,7 +63,7 @@ export default function AppHeader() {
             }
           </ul>
           <Menu>
-            <Menu.Button className="py-2 px-6 sm:hidden">
+            <Menu.Button className="py-2 px-4 flex mx-auto sm:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
               </svg>
