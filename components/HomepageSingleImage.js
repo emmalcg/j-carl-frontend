7dfe775508ff4aee039c8ac6462b5e065a2f2d11
Emma Green <emmalcgreen@gmail.com>
2024-04-27 12:47:59 -0400

    recent update

--- components/HomepageSingleImage.js
+++ components/HomepageSingleImage.js
@@ -3,14 +3,21 @@
 
 export default function HomepageSingleImage() {
   return (
-    <div style={{ height: `calc(100vh - 200px)` }}>
+    <div style={{ height: `calc(100vh - 200px)`, marginTop: "120px" }}>
       <a
         href="https://privateviews.artlogic.net/2/a6bfc670af5a781b652ec6/"
         className="hover:underline"
       >
         Conformity, April 29th - May 19th 2023 at Nicholas Metivier &#x2192;
       </a>
+
       <div className="relative h-[95%] mt-4">
+        <a
+          href="https://bordercrossingsmag.com/article/james-carl2"
+          className="hover:underline"
+        >
+          Exhibition Review &#x2192;
+        </a>
         {/*<Image
           priority={true}
           src={marble}
