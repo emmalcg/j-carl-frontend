e92d3ff3af3d81affeac2e6c7554e45cfdce3f19
Emma Green <emmalcgreen@gmail.com>
2024-11-03 17:49:07 -0500

    update that annoying thing

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -74,6 +74,8 @@ export default function AppHeader({
     router.pathname.includes("/rfq") ? setIsRFQ(true) : setIsRFQ(false);
   }, [router.pathname, currentType]);
 
+  console.log({router})
+
   return (
     <header className="text-xs xs:text-sm sm:text-base pt-8 sm:pt-8 pb-4 fixed bg-white min-w-full">
       {!isRFQ && (
@@ -94,72 +96,76 @@ export default function AppHeader({
 
               <nav className="grow">
                 <ul className="flex w-full items-end">
-                  {
-                    mainNav.map((item, i) => {
-                      let onPath =
-                        item.slug ===
-                        router.pathname;
+                  {mainNav.map((item, i) => {
+                    let onPath = item.slug === router.pathname;
 
-                      if(item.slug === '/work-date' && router.pathname === '/work-title') {
-                        onPath = true
-                      }
+                    if (
+                      item.slug === "/work-date" &&
+                      router.pathname === "/work-title"
+                    ) {
+                      onPath = true;
+                    }
 
-                      return (
-                        <Fragment key={`${item.label}-${i}`}>
-                          <li
-                            key={`${item.label}-${i}`}
-                            className={`flex border border-black hover:bg-gray-200 ${
-                              onPath
-                                ? "bg-gray-200 relative pt-2 rounded-t-lg"
-                                : "border-l-0 "
-                            } ${i === mainNav.length - 1 && "rounded-tr-lg"} ${
-                              router.pathname.includes("writing") &&
-                              item.label == "Work" &&
-                              "border-r-0"
-                            } ${
-                              router.pathname.includes("recent") &&
-                              item.label == "About" &&
-                              "border-r-0"
-                            }`}
-                          >
-                            <Link href={item.slug}>
-                              <a
-                                target={item.target}
-                                className={`pl-2 py-2 pr-2 sm:pr-9 font-medium hover:underline ${
-                                  item.isExternal && `cursor-alias`
-                                } `}
-                              >
-                                {item.label}
-                              </a>
-                            </Link>
-                          </li>
-                          {item.label === "Work" &&
-                            router.pathname.includes("/work-") && (
-                              <div className="hidden sm:flex">
-                                {workSubLinks.map((item) => {
-                                  return (
-                                    <div
-                                      key={item.label}
-                                      className="border border-black border-l-0 text-xs sm:text-sm pl-2 py-2 pr-2 sm:pr-9"
-                                    >
-                                      <span className="invisible">
-                                        {item.label}
-                                      </span>
-                                    </div>
-                                  );
-                                })}
-                              </div>
-                            )}
-                        </Fragment>
-                      );
-                    })}
+                    return (
+                      <Fragment key={`${item.label}-${i}`}>
+                        <li
+                          key={`${item.label}-${i}`}
+                          className={`flex border border-black hover:bg-gray-200 ${
+                            onPath
+                              ? "bg-gray-200 relative pt-2 rounded-t-lg"
+                              : "border-l-0 "
+                          } ${i === mainNav.length - 1 && "rounded-tr-lg"} ${
+                            router.pathname.includes("writing") &&
+                            item.label == "Work" &&
+                            "border-r-0"
+                          } ${
+                            router.pathname.includes("recent") &&
+                            item.label == "About" &&
+                            "border-r-0"
+                          }`}
+                        >
+                          <Link href={item.slug}>
+                            <a
+                              target={item.target}
+                              className={`pl-2 py-2 pr-2 sm:pr-9 font-medium hover:underline ${
+                                item.isExternal && `cursor-alias`
+                              } `}
+                            >
+                              {item.label}
+                            </a>
+                          </Link>
+                        </li>
+                        {item.label === "Work" &&
+                          router.pathname.includes("/work-") && (
+                            <div className="hidden sm:flex">
+                              {workSubLinks.map((item) => {
+                                return (
+                                  <div
+                                    key={item.label}
+                                    className="border border-black border-l-0 text-xs sm:text-sm pl-2 py-2 pr-2 sm:pr-9"
+                                  >
+                                    <span className="invisible">
+                                      {item.label}
+                                    </span>
+                                  </div>
+                                );
+                              })}
+                            </div>
+                          )}
+                      </Fragment>
+                    );
+                  })}
                 </ul>
               </nav>
             </div>
           </div>
           {router.pathname.startsWith("/work-") && (
             <div className="flex">
-              <div className="w-[103px] sm:w-[209px]"></div>
+              <div
+                className={`w-[103px] ${
+                  router.pathname === "/work-title" ? "sm:w-[210px]" : "sm:w-[209px]"
+                }`}
+              ></div>
               <SubMenu nav={workSubLinks} currentPath={router.pathname} />
             </div>
           )}
